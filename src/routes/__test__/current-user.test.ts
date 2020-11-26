569b93ffd838c486d1b1e1fa227703a785f498db
Omar Malavé <omarmalave21@gmail.com>
2020-11-25 22:35:49 -0400

    fix current user

--- src/routes/__test__/current-user.test.ts
+++ src/routes/__test__/current-user.test.ts
@@ -4,7 +4,10 @@ import app from '../../app';
 it('responds with details about the current user', async () => {
   const cookie = await global.signup();
 
-  const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send()
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
     .expect(200);
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
